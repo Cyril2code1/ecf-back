42d344128488e2a86d899db979b9f9eb7ac2748d
Cyril2code1 <webdev.cyril@gmail.com>
2022-07-31 18:07:12 +0200

    modif conf

--- conf/db_const.php
+++ conf/db_const.php
@@ -1,5 +1,5 @@
 <?php 
-define ('DB_NAME', 'cw_trt_conseils');
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWRD', 'root');
+define ('DB_NAME', 'WhCre0xsDP');
+define('DB_HOST', 'remotemysql.com');
+define('DB_USER', 'WhCre0xsDP');
+define('DB_PASSWRD', '8OGBNgZXWI');
